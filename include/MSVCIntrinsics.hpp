87c9a66ff2542c85ee1f4884c0133a539c8b5a58
Reece W <me@reece.sx>
2018-11-17 19:56:43 +0000

    More intrins

--- include/MSVCIntrinsics.hpp
+++ include/MSVCIntrinsics.hpp
@@ -3,14 +3,14 @@
 XENUS_BEGIN_C
     #if !defined(DEFINED__InterlockedIncrement)
     #define DEFINED__InterlockedIncrement
-        long _InterlockedIncrement(
+        extern long _InterlockedIncrement(
             long *Addend
         );
     #endif
     
     #if !defined(DEFINED__InterlockedDecrement)
     #define DEFINED__InterlockedDecrement
-        long _InterlockedDecrement(
+        extern long _InterlockedDecrement(
             long *Addend
         );
     #endif 
@@ -21,5 +21,31 @@ XENUS_BEGIN_C
             long *a,
             long b
         );
+    #endif
+        
+    #if !defined(DEFINED__ReadWriteBarrier)
+        #define DEFINED__ReadWriteBarrier
+        extern void _ReadWriteBarrier(void);
+    #endif
+        
+    #if !defined(DEFINED___faststorefence)
+        #define DEFINED___faststorefence
+        extern void __faststorefence(void);
+    #endif
+        
+    #if !defined(DEFINED__InterlockedExchangeAdd)
+        #define DEFINED__InterlockedExchangeAdd
+        extern long _InterlockedExchangeAdd(
+            long volatile *Addend,
+            long          Value
+            );
+    #endif
+        
+    #if !defined(DEFINED__InterlockedExchange)
+        #define DEFINED__InterlockedExchange
+        extern long _InterlockedExchange(
+            long *Addend,
+            long Value
+            );
     #endif
 XENUS_END_C
